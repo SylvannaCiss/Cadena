7fa7e8e82d01b98fc21bad18f5528a6bcc68a847
SylvannaCiss <20243ds146@utez.edu.mx>
2025-05-08 09:41:31 -0600

    Añadi clas

--- Cadena.java
+++ Cadena.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
-public class Caracteres {
+public class Cadena {
     public static void main(String[]args){
-        String nombre;
+        
         Scanner entrada = new Scanner(System.in);
 
         System.out.println("escribe tu nombre: ");
         String nombre = entrada.nextLine(); 
 
-        String apellido; 
-        Scanner entrada = new Scanner(System.in);
+    
+        
 
         System.out.println("escribe tu apellido: ");
         String apellido = entrada.nextLine(); 
 
-        System.out.println("Tu nombre completo es: " + nombre + apellido);
+        System.out.println("Tu nombre completo es: " + nombre " " + apellido);
 
 
 
